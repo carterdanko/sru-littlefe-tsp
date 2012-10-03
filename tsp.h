44fbbac27a31d35a81d3ae69609228922b271d07
Mike <mike@mike-Dell-DXP051.(none)>
2012-10-03 15:57:20 -0400

    Modified fitnessfunction.c in order to comply with the city_t struct

--- tsp.h
+++ tsp.h
@@ -10,6 +10,7 @@
  
 #define MAX_CITIES 100
 #define MAX_TOUR 100     // this should basically be the same as MAX_CITIES
+#define TABLE_SIZE (MAX_CITIES*(MAX_CITIES-1))/2 // size based on a counting argument
 
 /**
  * represents a city that must be visited to create a complete tour
@@ -26,6 +27,13 @@ typedef struct {
 	city_t* city[MAX_TOUR]; // a pointer to each city in the tour
 	int size; // size of the tour
 } tour_t;
+
+/** Data structure containing the coordinates of every city. */
+city_t cities[MAX_CITIES];
+/** Stores distances from every point to another. */
+float distTable[TABLE_SIZE];
+
+
 
 tour_t* loadCities(const char* const fileName); // loads cities from file
 void freeCities(tour_t* cities); // frees the memory used by the structure
